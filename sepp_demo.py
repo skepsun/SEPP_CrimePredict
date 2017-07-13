246bb88c780e0c9e853319f12cff51249ff96bc5
skepsun <chuxiongsun@gmail.com>
2017-07-13 16:36:06 +0800

    Add files via upload

--- sepp_demo.py
+++ sepp_demo.py
@@ -39,9 +39,9 @@ def cleaner(df, index):
 
 def read_data(file_path, index):
     raw = pd.read_csv(file_path)
-#    raw = raw[0:300]
+    raw = raw[0:400]
     cleaned_data = cleaner(raw, index)
-    cleaned_data = cleaned_data[4200:]
+    cleaned_data = cleaned_data
 #    cleaned_data.reset_index(drop=True)
     return cleaned_data
 
@@ -67,7 +67,10 @@ def sample_points(points, p):
 
     backgrounds = points[mask,:]
     triggered = points[~mask,:]
-    return backgrounds, triggered
+    cause_index= choice[~mask]
+    cause = points[cause_index,:]
+    interpoint_distances = triggered - cause
+    return backgrounds, interpoint_distances
 
 def p_matrix(points, background_kernel, trigger_kernel):
     """Computes the probability matrix.
@@ -147,32 +150,67 @@ def _normalise_matrix(p):
     column_sums = np.sum(p, axis=0)
     return p / column_sums
 
-def compute_kernel(x, mean, var, D, N=1):
-    result = 1
-    if len(mean.shape)==1:
-        feature_dimension = mean.shape[0]
-        token = False
-        if mean.shape[0] == 3:
-#            x[0]-mean[0]<240 and and np.sqrt((x[1]-mean[1])**2+(x[2]-mean[2])**2<0.1) 
-            if x[0]-mean[0]>=0 :
-                token = True
-        elif mean.shape[0] == 1:
-            token = True
-        elif mean.shape[0] == 2:
-            token = True
-        if token:
-            for j in range(0, feature_dimension):
-                result = result/(var[j]*np.sqrt(2*np.pi)*D)*np.exp(-(x[j]-mean[j])**2/(2*(var[j]**2)*(D**2)))
+def compute_kernel(points, mean, var, scale=1):
+    """pts is array of shape (N,k) where k is the dimension of space.
+
+    mean is array of shape (M,k) and var an array of shape (M,k)
+
+    For each point in `pts`: for each of i=1...M and each coord j=1...k
+    we compute the Gaussian kernel centred on mean[i][j] with variance var[i][j],
+    and then product over the j, sum over the i, and finally divide by M.
+
+    Returns an array of shape (N) unless N=1 when returns a scalar.
+    """
+    
+    if len(mean.shape) == 1:
+        # So k=1
+        mean = mean[:, None]
+        var = var[:, None]
+        if len(points.shape) == 0:
+            pts = np.array([points])[:, None]
         else:
-            result = 0
-            
-    elif len(mean.shape)==2:
-        result = 0
-        num_points = mean.shape[0]
-        for i in range(0, num_points):
-            result = result+compute_kernel(x, mean[i], var, D[i])
-    result = result/N
-    return result
+            pts = points[:, None]
+    else:
+        # k>1 so if points is 1D it's a single point
+        if len(points.shape) == 1:
+            pts = points[None, :]
+        else:
+            pts = points
+
+    # x[i][j] = (pts[i] - mean[j])**2   (as a vector)
+    x = (pts[:,None,:] - mean[None,:,:]) ** 2
+    var_broad = var[None,:,:] * 2.0
+    if var_broad.min()==0:
+        print('var_broad is: ',var_broad)
+    x = np.exp( - x / var_broad ) / np.sqrt((np.pi * var_broad))
+    return_array = np.mean(np.product(x, axis=2), axis=1)*scale
+    return return_array if pts.shape[0] > 1 else return_array[0]
+#    result = np.ones(x.shape)
+#    for num in range(0, x.shape[-1]):
+#        if len(mean.shape)==1:
+#            feature_dimension = mean.shape[0]
+#            token = False
+#            if mean.shape[0] == 3:
+#    #            x[0]-mean[0]<240 and and np.sqrt((x[1]-mean[1])**2+(x[2]-mean[2])**2<0.1) 
+#                if x[num][0]-mean[0]>=0 and x[num][0]-mean[0]<120*24 and np.sqrt((x[num][1]-mean[1])**2+(x[num][2]-mean[2])**2<0.1):
+#                    token = True
+#            elif mean.shape[0] == 1:
+#                token = True
+#            elif mean.shape[0] == 2:
+#                token = True
+#            if token:
+#                for j in range(0, feature_dimension):
+#                    result = result/(var[j]*np.sqrt(2*np.pi)*D)*np.exp(-(x[num][j]-mean[j])**2/(2*(var[j]**2)*(D**2)))
+#            else:
+#                result = 0
+#                
+#        elif len(mean.shape)==2:
+#            result[num] = 0
+#            num_points = mean.shape[0]
+#            for i in range(0, num_points):
+#                result[num] = result[num]+compute_kernel(x[num], mean[i], var, D[i])
+#        result[num] = result[num]/N
+#    return result
 
 
 def compute_kth_distance(points, k):
@@ -182,11 +220,11 @@ def compute_kth_distance(points, k):
 #    print(triggered)
     kth_distance = np.empty(points.shape[0])
 #    if points.shape[0]>1:
-    stds = np.std(points, axis = 0)
-    stds = stds[None,:]
-    final_stds = stds[0]
+    stds = np.std(points, axis = 0, ddof = 1)
+#    stds = stds[None,:]
+#    final_stds = stds[0]
 #    print(stds)
-    scaled_points = points/stds
+    scaled_points = points/stds[None,:]
 #    print(scaled_trigger)
     tree = _spatial.KDTree(scaled_points)
     kth_distance = np.empty(scaled_points.shape[0])
@@ -196,17 +234,18 @@ def compute_kth_distance(points, k):
 #    else:
 #        kth_distance = np.ones(points.shape[1])*1
 #        final_stds = np.ones(points.shape[1])*0.00001
-    return [kth_distance+eps, final_stds]
+    return [kth_distance+eps, stds]
 
-def predict(test_point, backgrounds, triggered, stds_nu, stds_mu, stds_trig, k_dist_nu, k_dist_mu, k_dist_trig, num_backgrounds, num_triggered):
+def predict(test_point, backgrounds, triggered, interpoint_distances, stds_nu, stds_mu, stds_trig, k_dist_nu, k_dist_mu, k_dist_trig, num_backgrounds, num_triggered):
     num_points = num_backgrounds + num_triggered
-    pred_b = compute_kernel(np.array([test_point[0,0]]), backgrounds[:,0][:,None], stds_nu, k_dist_nu, 1)
-    pred_b = pred_b*compute_kernel(test_point[0,1:3], backgrounds[:,1:3], stds_mu, k_dist_mu, 1)
-    pred_t = compute_kernel(test_point[0,0:3], triggered[:,0:3], stds_trig, k_dist_trig, 1)
+    pred_b = compute_kernel(np.array([test_point[0,0]]), backgrounds[:,0][:,None], (np.tensordot(stds_nu, k_dist_nu,axes=0)**2).transpose(), 1)
+    pred_b = pred_b*compute_kernel(test_point[0,1:3], backgrounds[:,1:3], (np.tensordot(stds_mu, k_dist_mu,axes=0)**2).transpose(), num_backgrounds)
+    for i in range(0, num_points):
+        pred_t = compute_kernel(test_point[0,0:3] - points[i,:], interpoint_distances[:,0:3], (np.tensordot(stds_trig, k_dist_trig,axes=0)**2).transpose(), num_triggered/num_points)
     
     return [pred_b+pred_t, pred_b, pred_t]
 
-iter_num = 10
+iter_num = 20
 k_high = 100
 k_low = 15
 #file_path = 'F:/Learning/NLP/predictive policing/case_new.csv'
@@ -214,38 +253,44 @@ file_path = 'F:/Crimes/xingshi_new.csv'
 points = read_data(file_path, ['Month','Day','Hour','X','Y'])
 #points = np.array([[1,10,20],[2,21,41],[3,45,22],[4,24,54],[5,86,26],[6,28,64],[7,43,29]])
 num_points = points.shape[0]
-p = initial_p_matrix(points, initial_time_bandwidth = 240, initial_space_bandwidth = 0.1)
+p = initial_p_matrix(points, initial_time_bandwidth = 10, initial_space_bandwidth = 0.1)
 g = np.zeros([num_points,num_points])
 mu = np.ones([num_points,])
 nu = np.ones([num_points,])
 
 for i in range(0,iter_num):
-    [backgrounds, triggered] = sample_points(points,p)
+    [backgrounds, interpoint_distances] = sample_points(points,p)
     print(backgrounds.shape[0])
     num_backgrounds = backgrounds.shape[0]
     num_triggered = num_points - num_backgrounds
-    [k_dist_trig, stds_trig] = compute_kth_distance(triggered, k_low)
+    [k_dist_trig, stds_trig] = compute_kth_distance(interpoint_distances, k_low)
     [k_dist_nu, stds_nu] = compute_kth_distance(backgrounds[:,0][:,None], k_high)
     [k_dist_mu, stds_mu] = compute_kth_distance(backgrounds[:,1:3], k_low)
-    p = np.zeros([num_points,num_points])
+    p_new = np.zeros([num_points, num_points])
+    nu = compute_kernel(points[:,0][:,None], backgrounds[:,0][:,None], (np.tensordot(stds_nu, k_dist_nu,axes=0)**2).transpose(), 1)
+    mu = compute_kernel(points[:,1:3], backgrounds[:,1:3], (np.tensordot(stds_mu, k_dist_mu,axes=0)**2).transpose(), num_backgrounds)
+    p_new  = np.diag(nu*mu)
     for i in range(0, num_points):
-        nu[i] = compute_kernel(np.array([points[i,0]]), backgrounds[:,0][:,None], stds_nu, k_dist_nu, 1)
-        mu[i] = compute_kernel(points[i,1:3], backgrounds[:,1:3], stds_mu, k_dist_mu, 1)
-        p[i,i] = nu[i]*mu[i]
         if i>0:
-            for j in range(0, i):
-                g[j, i] = compute_kernel(points[j][:,None], points[i][:,None], stds_trig, k_dist_trig, 1)
-                p[j, i] = g[j, i]
-    print(p)
-    p = _normalise_matrix(p)
-np.save('F:/Crimes/sepp.npy',[backgrounds, triggered, stds_nu, stds_mu, stds_trig, k_dist_nu, k_dist_mu, k_dist_trig, num_backgrounds, num_triggered])
-test_point1 = np.array([[3930, 116.483187, 39.956]])
-test_point2 = np.array([[3950, 116.483187, 39.956]])
-pred1 = predict(test_point1, backgrounds, triggered, stds_nu, stds_mu, stds_trig, k_dist_nu, k_dist_mu, k_dist_trig, num_backgrounds, num_triggered)
-pred2 = predict(test_point2, backgrounds, triggered, stds_nu, stds_mu, stds_trig, k_dist_nu, k_dist_mu, k_dist_trig, num_backgrounds, num_triggered)
+            d = points[i] - points[:i]
+            g[:i, i] = compute_kernel(d, interpoint_distances, (np.tensordot(stds_trig, k_dist_trig,axes=0)**2).transpose(), num_triggered/num_points)
+            p_new[:i, i] = g[:i, i]
+    print(p_new)
+    p = _normalise_matrix(p_new)
+np.save('F:/Crimes/seppS_1.npy',[points, backgrounds, interpoint_distances, stds_nu, stds_mu, stds_trig, k_dist_nu, k_dist_mu, k_dist_trig, num_backgrounds, num_triggered])
+test_point1 = np.array([[24*14, 116.581, 40]])
+test_point2 = np.array([[24*15, 116.483187, 39.956]])
+pred1 = predict(test_point1, backgrounds, interpoint_distances, interpoint_distances, stds_nu, stds_mu, stds_trig, k_dist_nu, k_dist_mu, k_dist_trig, num_backgrounds, num_triggered)
+pred2 = predict(test_point2, backgrounds, interpoint_distances, interpoint_distances, stds_nu, stds_mu, stds_trig, k_dist_nu, k_dist_mu, k_dist_trig, num_backgrounds, num_triggered)
 print(pred1)
 print(pred2)
 #print(pred1/pred2)
 f1 = plt.figure(1)
 plt.scatter(points[:,1], points[:,2])
+
+f2 = plt.figure(2)
+plt.scatter(backgrounds[:,1], backgrounds[:,2])
+
+f2 = plt.figure(3)
+plt.scatter(interpoint_distances[:,1], interpoint_distances[:,2])
 plt.show()
